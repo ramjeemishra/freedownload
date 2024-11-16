d6efb0ad645b715673ca226d8c8d9a23f787d205
Ramjee Mishra <131847922+ramjeemishra@users.noreply.github.com>
2024-11-16 21:19:49 +0530

    Add files via upload

--- freedownload.py
+++ freedownload.py
@@ -1,12 +1,8 @@
 import yt_dlp
 import re
 
-# Function to detect if URL is from YouTube or Instagram
 def detect_url_type(url):
-    # Regex pattern for YouTube (standard videos and YouTube Shorts)
     youtube_pattern = r"(https?://(?:www\.)?youtube\.com/(?:watch\?v=[\w-]+|shorts/[\w-]+))"
-    
-    # Regex pattern for Instagram (posts and Reels)
     instagram_pattern = r"(https?://(?:www\.)?instagram\.com/(?:p/[\w-]+|reel/[\w-]+))"
     
     if re.match(youtube_pattern, url):
@@ -16,11 +12,10 @@ def detect_url_type(url):
     else:
         return None
 
-# Function to download the video using yt-dlp
 def download_video(url, platform):
     ydl_opts = {
-        'outtmpl': '%(title)s.%(ext)s',  # Save with the title as the filename
-        'format': 'best',  # Download the best quality
+        'outtmpl': '%(title)s.%(ext)s', 
+        'format': 'best',
     }
     
     if platform == 'YouTube':
@@ -33,8 +28,6 @@ def download_video(url, platform):
             ydl.download([url])
     except Exception as e:
         print(f"Error: {e}")
-
-# Main function to execute the program
 def main():
     url = input("Enter the video URL (Instagram or YouTube): ").strip()
     
